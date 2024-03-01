6b1f63da7da3afb78dd8301499928d01dd7db850
Scott Blankenship <scott.blankenship00@outlook.com>
2024-03-01 12:11:52 -0500

    modified camera balloon detection

--- sensors/sensors/camera.py
+++ sensors/sensors/camera.py
@@ -17,92 +17,75 @@ class CamNode(Node): #Creating a Node
         self.create_timer(0.2, self.publish_cam_data) #calls function every 0.2 seconds
         self.minimum_radius = 20
 
-    def publish_cam_data(self):
-		
-        cap = cv2.VideoCapture(0)
-        cap.set(3, 640)  # x-axis
-        cap.set(4, 480)  # y-axis
+        self.cap = cv2.VideoCapture(0)
+        self.cap.set(3, 640)  # x-axis
+        self.cap.set(4, 480)  # y-axis
 
-        if not cap.isOpened():
+        if not self.cap.isOpened():
             print("Error: Could not open video source.")
             return
         
-        stored_coordinates = [] #List to longterm store detected_coordinates
-        frame_count = 0
-        total_x = 0
-        total_y = 0
+        self.frame_count = 0
+        self.total_x = 0
+        self.total_y = 0
 
-        while True:
-            ret, frame = cap.read()
+    def publish_cam_data(self):
+        ret, frame = self.cap.read()
+        
+        if not ret:
+            print("Error: Could not read frame.")
 
-            if not ret:
-                print("Error: Could not read frame.")
-                break
-
-            hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-            lower_bound_1 = np.array([56, 41, 155])
-            upper_bound_1 = np.array([76, 81, 255])
-
-            # lower_bound_2 = np.array([116, 16, 154]) 
-            # upper_bound_2 = np.array([136, 116, 254])
+        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        lower_bound_1 = np.array([56, 41, 155])
+        upper_bound_1 = np.array([76, 81, 255])
             
-            lower_bound_2 = np.array([116, 16, 154]) 
-            upper_bound_2 = np.array([136, 116, 254])
+        lower_bound_2 = np.array([116, 16, 154]) 
+        upper_bound_2 = np.array([136, 116, 254])
 
-            mask_1 = cv2.inRange(hsv_frame, lower_bound_1, upper_bound_1)
-            mask_2 = cv2.inRange(hsv_frame, lower_bound_2, upper_bound_2)
+        mask_1 = cv2.inRange(hsv_frame, lower_bound_1, upper_bound_1)
+        mask_2 = cv2.inRange(hsv_frame, lower_bound_2, upper_bound_2)
 
-            contours_1, _ = cv2.findContours(mask_1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            contours_2, _ = cv2.findContours(mask_2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            all_contours = contours_1 + contours_2
+        contours_1, _ = cv2.findContours(mask_1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours_2, _ = cv2.findContours(mask_2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        all_contours = contours_1 + contours_2
 
-            largest_contour = None
-            largest_contour_area = 0
+        largest_contour = None
+        largest_contour_area = 0
 
-            for contour in all_contours:
-                contour_area = cv2.contourArea(contour)
-                if contour_area > largest_contour_area:
-                    largest_contour = contour
-                    largest_contour_area = contour_area
+        for contour in all_contours:
+            contour_area = cv2.contourArea(contour)
+            if contour_area > largest_contour_area:
+                largest_contour = contour
+                largest_contour_area = contour_area
 
-            detected_coordinates = []  # List to store detected circle coordinates
+        detected_coordinates = []  # List to store detected circle coordinates
 
-            if largest_contour is not None:
-                (x, y), radius = cv2.minEnclosingCircle(largest_contour)
-                center = (int(x), int(y))
-                radius = int(radius)
-                if radius >= self.minimum_radius:
-                    x_direction = "Left" if center[0] > 320 else "Right" if center[0] < 320 else "Center"
-                    y_direction = "Up" if center[1] < 240 else "Down" if center[1] > 240 else "Center"
-                    detected_coordinates.append((center[0], center[1], x_direction, y_direction))
-                    stored_coordinates.append((center[0], center[1], x_direction, y_direction))
-                    cv2.circle(frame, center, radius, (0, 255, 0), 2)  # Draw a green circle on the frame
+        if largest_contour is not None:
+            (x, y), radius = cv2.minEnclosingCircle(largest_contour)
+            center = (int(x), int(y))
+            radius = int(radius)
+            if radius >= self.minimum_radius:
+                x_direction = "Left" if center[0] > 320 else "Right" if center[0] < 320 else "Center"
+                y_direction = "Up" if center[1] < 240 else "Down" if center[1] > 240 else "Center"
+                detected_coordinates.append((center[0], center[1], x_direction, y_direction))
+                cv2.circle(frame, center, radius, (0, 255, 0), 2)  # Draw a green circle on the frame
             
-            cv2.imshow('Detected Color', frame)
+        cv2.imshow('Detected Color', frame)
 
-            frame_count += 1     
+        self.frame_count += 1     
 
-            if frame_count % 10 == 0:
+        if self.frame_count % 10 == 0:
                 
-                for idx, (x, y, x_direction, y_direction) in enumerate(detected_coordinates):
-                    total_x = sum(x for x, _, _, _ in detected_coordinates)
-                    total_y = sum(y for _, y, _, _ in detected_coordinates)
-                    avg_x = total_x / len(detected_coordinates)
-                    avg_y = total_y / len(detected_coordinates)
-                    self.get_logger().info("X: " + str(avg_x) + ", Y: " + str(avg_y))
-                    msg = CameraCoord()
-                    msg.x_pos = avg_x
-                    msg.y_pos = avg_y
-                    self.cam_data.publish(msg)
-
-
-
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                cap.release()
-                cv2.destroyAllWindows()
-                break
-
+            for idx, (x, y, x_direction, y_direction) in enumerate(detected_coordinates):
+                self.total_x = sum(x for x, _, _, _ in detected_coordinates)
+                self.total_y = sum(y for _, y, _, _ in detected_coordinates)
+                avg_x = self.total_x / len(detected_coordinates)
+                avg_y = self.total_y / len(detected_coordinates)
+                self.get_logger().info("X: " + str(avg_x) + ", Y: " + str(avg_y))
+                msg = CameraCoord()
+                msg.x_pos = avg_x
+                msg.y_pos = avg_y
+                self.cam_data.publish(msg)
 
 
 
