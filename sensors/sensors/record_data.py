35bde758b1688a5bc860c287782ef379bd121c5f
RedLobsterCBB <scott.blankenship00@outlook.com>
2024-03-20 20:02:53 -0400

    Record data for PI tuner in matlab

--- sensors/sensors/record_data.py
+++ sensors/sensors/record_data.py
@@ -3,47 +3,53 @@ from rclpy.node import Node
 import math
 import time
 #from std_msgs.msg import Float32MultiArray #also have to add dependcy package into package.xml
-from blimp_interfaces.msg import ImuData
 from blimp_interfaces.msg import EscInput
-
+from blimp_interfaces.msg import CameraCoord
+from sensor_msgs.msg import Joy
 
 class RecordDataNode(Node): #Creating a Node
     
     def __init__(self): #initiating node
 
-        self.accel = [0,0,0]
-        self.gyro = [0,0,0]
-        self.euler = [0,0,0]
-        self.motor = [0,0,0,0]
-
+        
+        self.Manual_mode  = True
         super().__init__('recording_node') #naming node 'recording_data'
 
         self.subscriber = self.create_subscription(
-			ImuData, "imu_data", self.callback_record_imu, 10
+			CameraCoord, "cam_data", self.callback_record_cam, 10
 		)
         self.subscriber = self.create_subscription(
 			EscInput, "ESC_input", self.callback_record_esc, 10
 		)
 
+        self.subscriber = self.create_subscription(
+			Joy, "joy", self.callback_record_data, 10
+		)
+
         self.create_timer(0.2, self.callback_record_data) #calls function every 0.2 seconds
         self.start_time = time.time()
     
-    def callback_record_imu(self,msg):
-        self.accel = msg.imu_lin_accel
-        self.gyro = msg.imu_gyro
-        self.euler = msg.imu_euler
+    def callback_record_cam(self,msg):
+        self.cam = msg.position
 
     def callback_record_esc(self,msg):
         self.motor = msg.esc_pwm
 
-    def callback_record_data(self):
-        dt = time.time() - self.start_time
-        f = open("data_record_6.txt", 'a')
-        f.write("Euler Angles: {},{},{}".format(self.euler[0],self.euler[1],self.euler[2]) + " " + "Gyro: {},{},{}".format(self.gyro[0],self.gyro[1],self.gyro[2]) + " " + 
-                "Linear Accel: {},{},{}".format(self.accel[0],self.accel[1],self.accel[2]) + " " + "L Motor: {}".format(self.motor[0]) + " " +
-                "R Motor: {}".format(self.motor[1]) + " " + "U Motor: {}".format(self.motor[2]) + " " + 
-                "D Motor: {}".format(self.motor[3]) + " " + "time: {}".format(dt) + "\n")
-        f.close()
+    def callback_record_data(self,msg):
+
+        if msg.buttons[0]  == 1:
+            self.Manual_mode = not self.Manual_mode
+            time.sleep(2)
+            self.get_logger().info("Manual Mode is " + str(self.Manual_mode))
+            self.start_time = time.time()
+
+        if self.manual_mode is False:
+            dt = time.time() - self.start_time
+            f = open("PI_Data_1.txt", 'a')
+            f.write("Position: {},{}".format(self.position[0],self.position[1])  + " L Motor: {}".format(self.motor[0]) + " " +
+                    "R Motor: {}".format(self.motor[1]) + " " + "U Motor: {}".format(self.motor[2]) + " " + 
+                    "D Motor: {}".format(self.motor[3]) + " " + "time: {}".format(dt) + "\n")
+            f.close()
 
         
 def main(args=None):
