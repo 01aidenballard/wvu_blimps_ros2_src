21e4e3f012d266b03a1eb81b731f44e525ac97e2
jalzone <joshalcaswell@yahoo.com>
2024-04-02 16:59:11 -0400

    Improved imu script to check for NaNs. Published Data in msg should still be in the form of a list.

--- sensors/sensors/imu.py
+++ sensors/sensors/imu.py
@@ -7,7 +7,6 @@ import busio
 import adafruit_bno055
 import numpy as np
 import math
-#from std_msgs.msg import Float32MultiArray #also have to add dependcy package into package.xml
 from blimp_interfaces.msg import ImuData
 
 
@@ -15,9 +14,7 @@ class ImuNode(Node): #Creating a Node
 
 	def __init__(self): #initiating node
 		super().__init__('imu_node') #naming node 'imu_node'
-        # If you are going to use UART uncomment these lines
-        # self.uart = self.board.UART()
-        # self.sensor = adafruit_bno055.BNO055_UART(uart)
+        # The BNO055 setup here is for i2c protocol. If you want to start using uart, you need to look it up.
         
 		self.imu_data = self.create_publisher(ImuData,"imu_data",10) #Initializing publisher (message type,name,Qsize(some buffer thing:10 messages before it erases last one)S)
 		self.i2c = busio.I2C(board.SCL, board.SDA)
@@ -25,38 +22,46 @@ class ImuNode(Node): #Creating a Node
 		self.sensor.mode = adafruit_bno055.M4G_MODE
 		self.sensor.mode = adafruit_bno055.NDOF_MODE
 		self.create_timer(0.2, self.publish_imu_data) #calls function every 0.2 seconds
+		# initializing lists of saved variables and send variables
+		self.prev_lin_accel = [0 ,0 ,0]
+		self.prev_gyro = [0 ,0 ,0]
+		self.prev_euler = [0 ,0 ,0]
+		self.imu_lin_accel = [0 ,0 ,0]
+		self.imu_gyro = [0 ,0 ,0]
+		self.imu_euler = [0 ,0 ,0]
+
 		self.start_time = time.time()
 
 	def publish_imu_data(self):
-    #def imu_read(self):
-	#	i2c = board.I2C()  # uses board.SCL and board.SDA
-        # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroll
-	#	sensor = adafruit_bno055.BNO055_I2C(i2c)
-	#	sensor.mode=adafruit_bno055.M4G_MODE
-        #time.sleep(2)
-	#	sensor.mode=adafruit_bno055.NDOF_MODE
-        # If you are going to use UART uncomment these lines
-        # uart = board.UART()
-        # sensor = adafruit_bno055.BNO055_UART(uart)
-        #linear_acceleration = self.BNO055_I2C.get_linear_acceleration
-        #linear_acceleration = self.sensor.get_linear_acceleration
-        #msg.linear_acceleration.x = linear_acceleration[0]
-        #self.send_imu_command_pub_.publish(msg)
-        #M_yaw = math.atan2(-sensor.magnetic[0],sensor.magnetic[1])
-        #print("Temperature: {}".format(sensor.temperature))
-        #print("Euler Angles: {}".format(sensor.euler)) #prints (Heading,Roll,Pitch)
-        #print("Quaternion: {}".format(sensor.quaternion))
-        #print("Temperature: {}".format(sensor.temperature))
-        #print("Magnetometer: {}".format(sensor.magnetic))
-        #msg = Imu()
-        #msg.data = sensor.eulerself.publisher_.publish(msg)
 		msg = ImuData()
 		lin_accel = self.sensor.linear_acceleration
 		gyro = self.sensor.gyro
 		euler = self.sensor.euler
-		msg.imu_lin_accel = [lin_accel[0],lin_accel[1],lin_accel[2]]
-		msg.imu_gyro = [gyro[0],gyro[1],gyro[2]]
-		msg.imu_euler = [euler[0],euler[1],euler[2]]
+		#Check accelerations for NaNs
+		for i in enumerate(lin_accel):
+			if math.isnan(i[1]):
+				self.imu_lin_accel[i[0]] = self.prev_lin_accel[i[0]]
+			else:
+				self.imu_lin_accel[i[0]] = lin_accel(i[0])
+				self.prev_lin_accel[i[0]] = lin_accel(i[0])
+		#Check gyros for NaNs
+		for i in enumerate(gyro):
+			if math.isnan(i[1]):
+				self.imu_gyro[i[0]] = self.prev_gyro[i[0]]
+			else:
+				self.imu_gyro[i[0]] = gyro(i[0])
+				self.prev_gyro[i[0]] = gyro(i[0])
+		#Check Eulers for NaNs
+		for i in enumerate(euler):
+			if math.isnan(i[1]):
+				self.imu_euler[i[0]] = self.prev_euler[i[0]]
+			else:
+				self.imu_euler[i[0]] = euler(i[0])
+				self.prev_euler[i[0]] = euler(i[0])
+		
+		msg.imu_lin_accel = self.imu_lin_accel
+		msg.imu_gyro = self.imu_gyro
+		msg.imu_euler = self.imu_euler
 	#	self.get_logger().info(str(msg.imu_euler)) # Displays data on command line
 		self.imu_data.publish(msg)
         
