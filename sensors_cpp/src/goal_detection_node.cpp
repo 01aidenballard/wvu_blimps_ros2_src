ef670b5cf5ed21f4ea1cc9b26bf2f99bbcdf956e
agb00033 <147870922+agb00033@users.noreply.github.com>
2024-04-11 17:50:32 -0400

    Update goal_detection_node.cpp

--- sensors_cpp/src/goal_detection_node.cpp
+++ sensors_cpp/src/goal_detection_node.cpp
@@ -3,23 +3,17 @@
 #include <rclcpp/rclcpp.hpp>
 #include <blimp_interfaces/msg/camera_coord.hpp>
 
-
-class GoalDetectNode: public rclcpp::Node {
-
-
+class GoalDetectNode : public rclcpp::Node {
 public:
-
-    GoalDetectNode(): Node("goal_detect_cpp") {
-
+    GoalDetectNode() : Node("goal_detect_cpp") {
         cap.open(0, cv::CAP_V4L2);
         cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
         cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
 
-        if(!cap.isOpened()) {
+        if (!cap.isOpened()) {
             RCLCPP_INFO(this->get_logger(), "Error: Could not open video source.");
             return;
         }
-
 
         rho = 1;
         theta = CV_PI / 180;
@@ -29,37 +23,29 @@ public:
 
         low_threshold = 250;
         high_threshold = 300;
-        
 
         total_x = 0;
         total_y = 0;
         total_lines = 0;
-        
-        cam_data_publisher = this->create_publisher<blimp_interfaces::msg::CameraCoord>("cam_data", 10);
-        timer_ = this->create_wall_timer(std::chrono::milliseconds(30),std::bind(&GoalDetectNode::timerCallback, this));
-        RCLCPP_INFO(this->get_logger(), "Goal Detection CPP has Started!");
 
+        cam_data_publisher = this->create_publisher<blimp_interfaces::msg::CameraCoord>("goal_data", 10);
+        timer_ = this->create_wall_timer(std::chrono::milliseconds(30), std::bind(&GoalDetectNode::timerCallback, this));
+        RCLCPP_INFO(this->get_logger(), "Goal Detection CPP has Started!");
     }
 
-
-
 public:
-
     void timerCallback() {
         cv::Mat frame;
         cap.read(frame);
-
-
 
         if (frame.empty()) {
             RCLCPP_INFO(this->get_logger(), "Error: Could not read frame.");
         }
 
-
         cv::Mat hsv_frame;
         cv::cvtColor(frame, hsv_frame, cv::COLOR_BGR2HSV);
 
-        cv::Scalar lower_bound = cv::Scalar(20, 80, 80);
+        cv::Scalar lower_bound = cv::Scalar(23, 80, 80);
         cv::Scalar upper_bound = cv::Scalar(45, 255, 255);
 
         cv::Mat mask_1;
@@ -71,36 +57,60 @@ public:
         std::vector<cv::Vec4i> linesP;
         cv::HoughLinesP(edges, linesP, rho, theta, threshold, min_line_length, max_line_gap);
 
+        std::vector<cv::Point> midpoints;
+
         if (!linesP.empty()) {
             for (size_t i = 0; i < linesP.size(); i++) {
                 cv::Vec4i line = linesP[i];
-                x1 = line[0], y1 = line[1], x2 = line[2], y2 = line[3];
-                mid_x = (x1 + x2) / 2;
-                mid_y = (y1 + y2) / 2;
+                int x1 = line[0], y1 = line[1], x2 = line[2], y2 = line[3];
+                int mid_x = (x1 + x2) / 2;
+                int mid_y = (y1 + y2) / 2;
 
-                total_x += mid_x;
-                total_y += mid_y;
+                midpoints.push_back(cv::Point(mid_x, mid_y));
+
+                //cv::line(frame, cv::Point(x1, y1), cv::Point(x2, y2), cv::Scalar(0, 255, 0), 2);
+            }
+
+            if (!midpoints.empty()) {
+                int max_x = INT_MIN, min_x = INT_MAX, max_y = INT_MIN, min_y = INT_MAX;
+
+                for (const auto &point : midpoints) {
+                    max_x = std::max(max_x, point.x);
+                    min_x = std::min(min_x, point.x);
+                    max_y = std::max(max_y, point.y);
+                    min_y = std::min(min_y, point.y);
+                }
+
+                center_x = (min_x + max_x) / 2;
+                center_y = (min_y + max_y) / 2;
                 total_lines++;
 
-                cv::line(frame, cv::Point(x1, y1), cv::Point(x2, y2), cv::Scalar(0, 255, 0), 2);
-	
+                //cv::circle(frame, cv::Point(max_x, max_y), 5, cv::Scalar(0, 0, 255), -1);
+                //cv::circle(frame, cv::Point(max_x, min_y), 5, cv::Scalar(0, 0, 255), -1);
+                //cv::circle(frame, cv::Point(min_x, max_y), 5, cv::Scalar(0, 0, 255), -1);
+                //cv::circle(frame, cv::Point(min_x, min_y), 5, cv::Scalar(0, 0, 255), -1);
+                //cv::circle(frame, cv::Point(center_x, center_y), 5, cv::Scalar(255, 0 , 0), -1);
+
+
             }
-            linesP.clear();
-            center_x = total_x / total_lines;
-            center_y = total_y / total_lines;
-            total_lines = 0;
-            total_x = 0;
-            total_y = 0;
-            cv::circle(frame, cv::Point(center_x, center_y), 5, cv::Scalar(0, 0, 255), 5);
-            std::cout << "Goal - X: " << center_x << ", Y: " << center_y << std::endl;
-            RCLCPP_INFO(this->get_logger(),"X: %d Y: %d", center_x, center_y);
+
+            if (total_lines % 5 == 0) {
+
+                auto msg = blimp_interfaces::msg::CameraCoord();
+                msg.position = {center_x, center_y};
+                cam_data_publisher->publish(msg);
+
+            }
+            midpoints.clear();
+            //std::cout << "Goal - X: " << center_x << ", Y: " << center_y << std::endl;
+            //RCLCPP_INFO(this->get_logger(), "X: %d Y: %d", center_x, center_y);
         }
 
-        cv::imshow("Detection", frame);
 
-        if (cv::waitKey(1)) {
-            return;
-        }
+
+        //if (cv::waitKey(1)) {
+            //return;
+        //}
 
 
     }
