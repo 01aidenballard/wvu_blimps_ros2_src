9760e2c39bd89acbe4bb1ea5762a4241c99186c1
Ethan Wright <45087309+01ethanwright@users.noreply.github.com>
2024-07-12 10:11:28 -0400

    added lidar to sensors setup

--- sensors/setup.py
+++ sensors/setup.py
@@ -23,6 +23,7 @@ setup(
             "read_altitude = sensors.barometer:main",
             "read_bno055 = sensors.BNO055:main",
             "read_bno085 = sensors.BNO085:main",
+            "read_lidar = sensors.Lidar:main",
         ],
     },
 )
